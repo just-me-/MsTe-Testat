0f968cc3538e42ed483c09c50f4c397100b195f3
just-me- <hess-marcel@bluewin.ch>
2018-12-15 12:03:51 +0100

    ach egal ich machs jetzt per id

--- AutoReservation.UI/MainWindow.xaml.cs
+++ AutoReservation.UI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AutoReservation.Common.DataTransferObjects;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,49 @@ namespace AutoReservation.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public AutoDto SelectedAuto { get; set; }
+
         public MainWindow()
         {
+            InitFormData();
+            this.DataContext = this;
             InitializeComponent();
         }
+
+        private void InitFormData()
+        {
+            SelectedAuto = new AutoDto();
+        }
+
+        // Events - Auto Tab 
+        private void AutoSaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Reset Form
+            SelectedAuto = null;
+        }
+        private void AutoSelectedListBox_OnMouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Entered"); 
+            var auto = listAutos.SelectedItem as AutoDto;
+            SelectedAuto = auto;
+        }
+
+
     }
+        /*
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (AvailableListBox.SelectedIndex < 0)
+            {
+                MessageBox.Show("Bitte etwas auswählen...");
+                return;
+            }
+            var user = AvailableListBox.SelectedItem as UserInfo;
+            if (!AddUserToSelection(user))
+                return;
+            AvailableListBox.SelectedIndex = -1;
+        } */
+        // helper
+
 }
